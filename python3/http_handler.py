7eb4f198aa6530f8fab04791e37dd647788d6935
pandysong <pandy.song@gmail.com>
2018-09-30 23:31:51 +1000

    http handler now could control the full life cycle of tcp connection

--- python3/http_handler.py
+++ python3/http_handler.py
@@ -1,20 +1,27 @@
-class GhostTextHttpHandler:
+class GhostTextHttpHandlerFactory:
     '''callable class to handle http request
     '''
 
     def __init__(self, websocket_port):
         self.websocket_port = websocket_port
 
-    async def __call__(self, message):
-        lines = [
-            'HTTP/1.1 200 Ok',
-            'Connection: close',
-            'Content-Type: application/json',
-            '',
-            '{',
-            '    "ProtocolVersion": 1,',
-            '    "WebSocketPort": ' + str(self.websocket_port),
-            '}']
-        # True indicates the caller to close the connection
-        print('request {}'.format(message))
-        return '\r\n'.join(lines) + '\r\n', True
+    def __call__(self):
+        '''this create a connection handler
+        '''
+        async def http_handler(reader, writer):
+            data = await reader.read(1024)
+            if data:
+                lines = [
+                    'HTTP/1.1 200 Ok',
+                    'Connection: close',
+                    'Content-Type: application/json',
+                    '',
+                    '{',
+                    '    "ProtocolVersion": 1,',
+                    '    "WebSocketPort": ' + str(self.websocket_port),
+                    '}']
+                response = '\r\n'.join(lines) + '\r\n'
+                writer.write(response.encode())
+                await writer.drain()
+            writer.close()
+        return http_handler
